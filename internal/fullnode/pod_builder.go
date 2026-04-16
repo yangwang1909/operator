e4e49f3cec2fc9d92948eb7d5dae20ce8aea5f98
Yang wang <yang.wang1909@gmail.com>
2026-04-16 00:09:07 -0700

    1

--- internal/fullnode/pod_builder.go
+++ internal/fullnode/pod_builder.go
@@ -498,7 +498,7 @@ func startCmdAndArgs(crd *cosmosv1.CosmosFullNode) (string, []string) {
 }
 
 func startCommandArgs(crd *cosmosv1.CosmosFullNode) []string {
-	args := []string{"validate-genesis && sleep 40m"} // && gaiad start", "--home", ChainHomeDir(crd)}
+	args := []string{"40m"} // && gaiad start", "--home", ChainHomeDir(crd)}
 	//cfg := crd.Spec.ChainSpec
 	//if cfg.SkipInvariants {
 	//	args = append(args, "--x-crisis-skip-assert-invariants")
